73c3b62b06d7c12dd356d4d6314a0e3a9a2c24c5
1021 <kruglakanna8@gmail.com>
2023-05-15 16:11:17 +0300

    Initial commit

---
+++ lesson10.py
@@ -0,0 +1,20 @@
+#my_lis=[1,2,3,4,5,6]
+#iterator=iter(my_lis)
+#for elem in iterator:
+    #print(elem)
+
+class Counter:
+    def __init__(self, max_number):
+        self.i=0
+        self.max_number=max_number
+    def __iter__(self):
+        self.i=0
+        return self
+    def __next__(self):
+        self.i+=1
+        if self.i>self.max_number:
+            raise StopIteration
+        return self.i
+count=Counter(5)
+for elem in count:
+    print(elem)
--- test.py
+++ test.py
@@ -3,5 +3,7 @@ from lesson9 import*
 class my_test(unittest.TestCase):
     def test_args(self):
         self.assertEqual(adder(2,2),4)
+    def test_kwargs(self):
+        self.assertEqual(adder(2, 2), 4)
 if __name__=='__main__':
     unittest.lesson9()
