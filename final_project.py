53a9d1c34d5b44e9d0410c241c4e24bf5dc69212
1021 <kruglakanna8@gmail.com>
2023-05-17 16:51:13 +0300

    Initial commit

---
+++ final_project.py
@@ -0,0 +1,57 @@
+import random
+class Anna:
+    def __init__(self, name='Anna',school=None, academy=None, rugby=None, cactus=None):
+        self.name=Anna
+        self.money=50
+        self.gladness=50
+        self.satiety=50
+        self.school=school
+        self.academy=academy
+        self.rugby=rugby
+        self.cactus=cactus
+        self.water=100
+    def get_school(self):
+        pass
+    def eat(self):
+        pass
+    def school(self):
+        pass
+    def academy(self):
+        pass
+    def chill(self):
+        pass
+    def clean_room(self):
+        pass
+    def homework(self):
+        pass
+    def academy(self):
+        pass
+    def rugby(self):
+        pass
+    def plant(self):
+        pass
+    def days_indexes(self, day):
+        day = f"Today the {day} of {self.name}'s life"
+        print(f'{day:=^50}', '\n')
+        Anna_indexes = self.name + "'s indexes"
+        print(f"{Anna_indexes:^50}", "\n")
+        print(f"Money - {self.money}")
+        print(f"Satiety - {self.satiety}")
+        print(f"Gladness - {self.gladness}")
+        home_indexes = 'Home indexes'
+        print(f'{home_indexes:^50}', '\n')
+        print(f'Food - {self.home.food}')
+        print(f'Mess - {self.home.mess}')
+    def is_alive(self):
+        pass
+    def live(self):
+        pass
+class Study:
+    def __init__(self, school=None, academy=None, rugby=None):
+        self.school=school
+        self.academy=academy
+        self.rugby=rugby
+anna=Anna(name='Anna')
+for day in range(365):
+    if anna.live(day)==False:
+        break
